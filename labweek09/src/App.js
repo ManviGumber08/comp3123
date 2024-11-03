def5e5903693bbac0940250d653e9f534b45a09e
ManviGumber08 <156446588+ManviGumber08@users.noreply.github.com>
2024-11-02 20:05:17 -0400

    Lab9

---
+++ labweek09/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <img 
+          src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" 
+          alt="React Logo" 
+          className="logo"
+        />
+        <h1>Welcome to Fullstack Development - I</h1>
+        <h2>React JS Programming Week09 Lab exercise</h2>
+        <p>101412099</p>
+        <p>Manvi Gumber </p>
+        <p>George Brown College, Toronto</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
